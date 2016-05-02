5d68a6cce0d823d1c8c1326c31b9bd220f3ed8cb
student3 <student3@jh-mbair-158-06.d214.org>
2016-05-02 14:42:28 -0500

    Created code for the HelpViewController

---
+++ BudgettingFinalApp/HelpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HelpViewController.swift
+//  BudgettingFinalApp
+//
+//  Created by student3 on 5/2/16.
+//  Copyright © 2016 JohnHerseyHighSchool. All rights reserved.
+//
+
+import UIKit
+
+class HelpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
