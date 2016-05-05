ad8f08b1c28e701750418d34069839b2eed7e715
student3 <student3@jh-mbair-158-08.d214.org>
2016-05-05 14:45:59 -0500

    Fixed tableview

--- BudgettingFinalApp/UserInfoViewController.swift
+++ BudgettingFinalApp/UserInfoViewController.swift
@@ -8,14 +8,28 @@
 
 import UIKit
 
-class UserInfoViewController: UIViewController, UITableViewDelegate {
+class UserInfoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        }
+    
+        func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 2
+        }
+        func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let currentCell = tableView.dequeueReusableCellWithIdentifier("myCell")!
+        currentCell.textLabel!.text = "Default User"
+        return currentCell
 
+        
     }
-
-   
 }
+        
+
+    
+    
+
