359b98cfc8b717c7c47e68e3eb7e45ec131f63ce
student3 <student3@jh-mbair-158-08.d214.org>
2016-05-12 14:13:42 -0500

    edited class and storyboard

--- BudgettingFinalApp/DetailsViewController.swift
+++ BudgettingFinalApp/DetailsViewController.swift
@@ -9,14 +9,29 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
+    
+    var newUser = Info()
+
     @IBOutlet weak var housingTextField: UITextField!
     @IBOutlet weak var transportationTextField: UITextField!
     @IBOutlet weak var taxesTextField: UITextField!
+    @IBOutlet weak var utilitiesTextField: UITextField!
+    @IBOutlet weak var foodTextField: UITextField!
+    @IBOutlet weak var insuranceTextField: UITextField!
+    @IBOutlet weak var debtTextField: UITextField!
+    @IBOutlet weak var HealthCareTextField: UITextField!
+    @IBOutlet weak var entertainmentTextField: UITextField!
+    @IBOutlet weak var charietyTextField: UITextField!
+    @IBOutlet weak var clothingTextField: UITextField!
+    @IBOutlet weak var educationTextField: UITextField!
+    @IBOutlet weak var miscTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        housingTextField.text = newUser.Housing
+        
 
     }
-
+    
    
 }
