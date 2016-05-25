ec71123386c4ee97ee1176efba119730c7f75c98
student3 <student3@jh-mbair-158-08.d214.org>
2016-05-25 14:03:47 -0500

    connected new textfields

--- BudgettingFinalApp/DetailsViewController.swift
+++ BudgettingFinalApp/DetailsViewController.swift
@@ -12,6 +12,18 @@ class DetailsViewController: UIViewController {
     
     var newUser = Info()
 
+    @IBOutlet weak var miscAmount: UITextField!
+    @IBOutlet weak var entertainmentAmount: UITextField!
+    @IBOutlet weak var healthcareAmount: UITextField!
+    @IBOutlet weak var foodAmount: UITextField!
+    @IBOutlet weak var utilitiesAmount: UITextField!
+    @IBOutlet weak var clothingAmount: UITextField!
+    @IBOutlet weak var charietyAmount: UITextField!
+    @IBOutlet weak var savingsAmount: UITextField!
+    @IBOutlet weak var insuranceAmount: UITextField!
+    @IBOutlet weak var transportationAmount: UITextField!
+    @IBOutlet weak var housingAmount: UITextField!
+    
     @IBOutlet weak var miscButton: UIButton!
     @IBOutlet weak var entertainmentButton: UIButton!
     @IBOutlet weak var healthcareButton: UIButton!
@@ -66,6 +78,82 @@ class DetailsViewController: UIViewController {
         
     }
     
+    @IBAction func housingAdd(sender: UIButton) {
+        var housingInt: Int? = Int(housingTextField.text!)
+        var housingAm: Int? = Int(housingAmount.text!)
+        
+        
+        
+        
+    }
     
+    @IBAction func transportationAdd(sender: UIButton) {
+    }
    
+    @IBAction func insuranceAdd(sender: UIButton) {
+    }
+    
+    @IBAction func savingsAdd(sender: UIButton) {
+    }
+    
+    @IBAction func charietyAdd(sender: UIButton) {
+    }
+    
+    @IBAction func clothingAdd(sender: UIButton) {
+    }
+    
+    @IBAction func utilitiesAdd(sender: UIButton) {
+    }
+    
+    @IBAction func foodAdd(sender: UIButton) {
+    }
+    
+    @IBAction func healthcareAdd(sender: UIButton) {
+    }
+    
+    @IBAction func entertainmentAdd(sender: UIButton) {
+    }
+    
+    @IBAction func miscAdd(sender: UIButton) {
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
